2b5b042a382bc7e4731f700dbdb56d093d1f9076
Rahul9214 <147570570+Rahul9214@users.noreply.github.com>
2024-04-03 15:11:02 +0530

    commit

--- XCountriesSearch/src/App.jsx
+++ XCountriesSearch/src/App.jsx
@@ -1,5 +1,40 @@
-import Countries from "./Countries";
+import { useEffect, useState } from 'react'
+import axios from 'axios';
+import './App.css'
+import Card from './Card';
 
-export default function App() {
-  return <Countries />;
+function App() {
+  const [apiData, setApiData] = useState([])
+  const [filteredData,setFilteredData] = useState([]);
+  useEffect(()=>{
+      axios.get("https://restcountries.com/v3.1/all")
+      .then((res)=>{
+        setApiData(res.data);
+        setFilteredData(res.data);
+      })
+      .catch((err)=>{
+        console.log(err);
+      })
+  },[])
+
+  const filter=(e)=>{
+    setFilteredData(apiData.filter((fl)=>{
+      return fl.name.common.toLowerCase().includes((e.target.value).toLowerCase());
+    }))
+  }
+  // console.log(filteredData);
+  return (
+    <div className='body'>
+      <div>
+        <input type='text' onChange={filter} placeholder='Search for countries...'/>
+      </div>
+      <div className='countryCard1'>
+      {filteredData.map((el)=>{
+        return (<Card data={el} key={el.cca3}/>)
+      })}
+      </div>
+    </div>
+  )
 }
+
+export default App;
---
+++ XCountriesSearch/src/Card.jsx
@@ -0,0 +1,13 @@
+import './Card.css';
+const Card = ({data}) => {
+  return (
+    <div className="countryCard">
+        <div className="flag">
+            <img src={data.flags.png} alt={data.flag.common}/>
+        </div>
+        <div className="cname">{data.name.common}</div>
+    </div>
+  )
+}
+
+export default Card;
